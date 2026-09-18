8bfd6998ef0c1b6b1bc457825ec297f1e65f94c8
abdourhd <rachedi.abdelghani1@gmail.com>
2026-09-18 14:55:35 +0100

    E-04 v1.4.1 - Fix major issue

--- Easy Projects/04-Unit-Converter/src/length.c
+++ Easy Projects/04-Unit-Converter/src/length.c
@@ -3,20 +3,6 @@
 double length_to_meter(double value, int unit) {
     switch(unit) {
         case 1: return value;
-        case 2: return value / 1000;
-        case 3: return value / 100;
-        case 4: return value * 1000;
-        case 5: return value * 39.3701;
-        case 6: return value * 3.281;
-        case 7: return value * 1.09361;
-        case 8: return value / 1609.34;
-        default: return -1;
-    }
-}
-
-double meter_to_length(double value, int unit) {
-    switch(unit) {
-        case 1: return value;
         case 2: return value * 1000;
         case 3: return value * 100;
         case 4: return value / 1000;
@@ -24,6 +10,20 @@ double meter_to_length(double value, int unit) {
         case 6: return value / 3.281;
         case 7: return value / 1.09361;
         case 8: return value * 1609.34;
+        default: return -1;
+    }
+}
+
+double meter_to_length(double value, int unit) {
+    switch(unit) {
+        case 1: return value;
+        case 2: return value / 1000;
+        case 3: return value / 100;
+        case 4: return value * 1000;
+        case 5: return value * 39.3701;
+        case 6: return value * 3.281;
+        case 7: return value * 1.09361;
+        case 8: return value / 1609.34;
         default: return -1;
     }
 }
